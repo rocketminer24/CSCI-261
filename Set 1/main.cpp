ff3a6f925c308cb7136d6a97a73e7370eecab598
rocketminer24 <46545697+rocketminer24@users.noreply.github.com>
2019-06-03 14:04:15 -0700

    Add files via upload

---
+++ Set 1/main.cpp
@@ -0,0 +1,31 @@
+/* CSCI 261 Lab 1C: ASCII Art Intro
+ *
+ * Author: Jarod Spencer
+ *
+ * Description: How to use ASCII characters to make art.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    //ASCII christmas tree
+
+    cout << "   *   " << endl;
+    cout << "  ***  " << endl;
+    cout << " ***** " << endl;
+    cout << "*******" << endl;
+    cout << "  ***  " << endl;
+    cout << " " << endl;
+
+
+    //ASCII cat
+
+    cout << "/\\   /\\ " << endl;
+    cout << "  o o  " << endl;
+    cout << " =   = " << endl;
+    cout << "  ---  " << endl;
+    return 0;
+}
